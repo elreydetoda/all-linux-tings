e0371cae9de3a525c7e7c848cb9d14c8a0fd68a2
Alex Rodriguez <unc741@gmail.com>
2017-02-13 04:37:42 -0500

    fixing xorgs package

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ read distro
 if (( distro == 1 )) ; then
 	sudo apt-get install zsh tmux powertop i3status i3lock xorg scrot feh rofi geoip-bin pianobar acpi whois espeak
 else
+	echo "################################################"
+	echo "#This isn't as up to date as the ubuntu install#"
+	echo "################################################"
 	sudo pacman -S zsh tmux powertop i3status i3lock xorg-xset
 fi
 
@@ -47,68 +50,7 @@ chsh -s /bin/zsh
 ## moving all the dot files to your home directory
 cp .* ~/
 
-##################################################################
-
-# creating script which is used in .xinitrc
-cat >  ~/scripts/lowbat.sh <<EOF
-#! /bin/bash
-
-SLEEP_TIME=5   # Default time between checks.
-SAFE_PERCENT=30  # Still safe at this level.
-DANGER_PERCENT=15  # Warn when battery at this level.
-CRITICAL_PERCENT=5  # Hibernate when battery at this level.
-
-NAGBAR_PID=0
-export DISPLAY=:0.0
-
-function launchNagBar
-{
-    i3-nagbar -m 'Battery low!' -b 'Hibernate!' 'pm-hibernate' >/dev/null 2>&1 &
-    NAGBAR_PID=$!
-}
-
-function killNagBar
-{
-    if [[ $NAGBAR_PID -ne 0 ]]; then
-        ps -p $NAGBAR_PID | grep "i3-nagbar"
-        if [[ $? -eq 0 ]]; then
-            kill $NAGBAR_PID
-        fi
-        NAGBAR_PID=0
-    fi
-}
-
-
-while [ true ]; do
-
-    killNagBar
-
-    if [[ -n $(acpi -b | grep -i discharging) ]]; then
-        rem_bat=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
-
-        if [[ $rem_bat -gt $SAFE_PERCENT ]]; then
-            SLEEP_TIME=10
-        else
-            SLEEP_TIME=5
-            if [[ $rem_bat -le $DANGER_PERCENT ]]; then
-                SLEEP_TIME=2
-                launchNagBar
-            fi
-            if [[ $rem_bat -le $CRITICAL_PERCENT ]]; then
-                SLEEP_TIME=1
-                pm-hibernate
-            fi
-        fi
-    else
-        SLEEP_TIME=10
-    fi
-
-    sleep ${SLEEP_TIME}m
-
-done
-EOF
-
-# giving it execute permissions
+# moving script to do low battery warning and giving it execute permission
 chmod +x ~/scripts/lowbat.sh
 
 ##################################################################
