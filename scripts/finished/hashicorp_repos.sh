c9de8584891a0c384216cdd855b59879f0a486ce
elreydetoda <unc741@gmail.com>
2021-02-07 21:43:35 -0500

    adding short link for hashicorp repos

--- scripts/finished/hashicorp_repos.sh
+++ scripts/finished/hashicorp_repos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # debian hashicorp repos from here: https://medium.com/hashicorp-engineering/hashicorp-packaging-is-here-4c6083dee4f6
+# short link: https://git.io/JtVlL
 
 set -${-//[sc]/}eu${DEBUG+xv}o pipefail
 
