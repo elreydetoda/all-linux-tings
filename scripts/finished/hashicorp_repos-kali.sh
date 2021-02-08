e1680c2c1eb3ca5778cc2ca1dc0d3a05929c130d
elreydetoda <unc741@gmail.com>
2021-02-07 21:50:14 -0500

    adding shortlink for kali hashi

--- scripts/finished/hashicorp_repos-kali.sh
+++ scripts/finished/hashicorp_repos-kali.sh
@@ -2,5 +2,7 @@
 
 set -${-//[sc]/}eu${DEBUG+xv}o pipefail
 
+# short url: https://git.io/JtVlq
+
 debian_version="$(curl -fsSL 'https://www.debian.org/releases/stable/' | grep 'Release Information' | grep '<h1>' | grep -oP ';.*&' | tr -d ';|&')"
 curl -fsSL 'https://git.io/JtVlL' | sed "s/\$(lsb_release.*)/${debian_version}/" | bash
