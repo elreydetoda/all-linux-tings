ed0bafa3f56c02903dfd4b3714544cf3a3f23094
elreydetoda <unc741@gmail.com>
2021-02-07 23:35:08 -0500

    handling urlencoding with curl

--- scripts/finished/gist_creator.sh
+++ scripts/finished/gist_creator.sh
@@ -30,13 +30,6 @@ function get_path_diff(){
   done
 }
 
-function url_encode(){
-  # was going to implement a bash version, but didn't feel like it...
-  #   https://gist.github.com/cdown/1163649
-  # https://www.urlencoder.io/python/
-  urlz="$(python3 -c "import urllib.parse; print('{}'.format(urllib.parse.quote('${1}',safe='')))")"
-}
-
 # https://elrey.casa/bash/scripting/main
 function main(){
   base_raw_url='https://raw.githubusercontent.com'
@@ -45,8 +38,8 @@ function main(){
   get_gh_repo
   branch="${BRANCH:-master}"
   get_path_diff "${1}"
-  url_encode "${base_raw_url}/${gh_repo}/${branch}${path_diff}/${raw_filez}"
-  printf 'https://git.io/%s\n' "$(curl -fsS 'https://git.io/create' --data-raw "url=${urlz}")"
+  urlz="${base_raw_url}/${gh_repo}/${branch}${path_diff}/${raw_filez}"
+  printf 'https://git.io/%s\n' "$(curl -fsS 'https://git.io/create' --data "url=${urlz}")"
 }
 
 if [[ "${0}" = "${BASH_SOURCE[0]}" ]] ; then
