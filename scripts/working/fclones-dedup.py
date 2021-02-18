9ec963db0f39092d057d09e605571eb9e4e67e59
elreydetoda <unc741@gmail.com>
2021-02-17 23:48:45 -0500

    potentially finished script

--- scripts/working/fclones-dedup.py
+++ scripts/working/fclones-dedup.py
@@ -15,6 +15,29 @@ def get_reference_file(file_path: str, key_word: str):
     if key_word in file_path:
         return file_path
 
+def check_dup(key_word: str, ref_file: str, potential_dup: str):
+    if not ref_file == potential_dup:
+        trimming = ref_file.split(key_word)[0]
+        comparing = ref_file.split(key_word)[1]
+        # print(trimming)
+        parsed_potential_dup = '/' + '/'.join(potential_dup.split(trimming)[1].split('/')[1:])
+
+        if comparing == parsed_potential_dup:
+            return potential_dup
+        else:
+            return None
+
+def remove_file(file_path: pathlib.Path):
+    try:
+        print("removing file: {}".format(file_path))
+        file_path.unlink()
+    except FileNotFoundError as e:
+        print("file didn't exist: {}".format(file_path))
+        print("proceeding on though...")
+        file_path.unlink(missing_ok=True)
+    except Exception as e:
+        print("unknown error happened: {}".format(file_path))
+        exit(2)
 
 def main():
     dedups_file = pathlib.Path('/storage/backups/laptop.bak/linux/personal/2021-02-17-dupes.json')
@@ -33,7 +56,12 @@ def main():
             ref_file = get_reference_file(file_path, keep_top_level_folder_key)
             if ref_file is None:
                 ref_file = old_ref
-            print(ref_file)
+
+            dup_file = check_dup(keep_top_level_folder_key, ref_file, file_path)
+
+            if dup_file:
+                remove_file(pathlib.Path(dup_file))
+
 
             old_ref = ref_file
 
