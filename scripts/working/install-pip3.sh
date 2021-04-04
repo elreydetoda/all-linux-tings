da0233f15817fca03c2d41738aa16de52aaa4580
elreydetoda <unc741@gmail.com>
2021-04-04 10:45:16 -0400

    trying different deps install function

--- scripts/working/install-pip3.sh
+++ scripts/working/install-pip3.sh
@@ -5,6 +5,69 @@ set -${-//[sc]/}eu${DEBUG+xv}o pipefail
 
 # git.io: https://git.io/Jt9mk
 
+# https://elrey.casa/bash/scripting/deps_check
+function deps_install(){
+
+  install_cmd=()
+
+  if [[ "${EUID}" -ne 0 ]]; then
+    install_cmd+=('sudo')
+  fi
+
+  # shellcheck disable=SC1091
+  ID="$( source /etc/os-release ; [ -n "${ID_LIKE:-}" ] && echo "${ID_LIKE}" || echo "${ID:-}" )"
+  packages=( 'curl' )
+  case "${ID}" in
+    *debian*)
+      packages+=( 'python3-distutils' )
+      package_manager='apt-get'
+      package_manager_install_cmd=('install' '-y')
+      package_manager_update_cmd=( 'update' )
+      ;;
+    alpine)
+      packages+=()
+      package_manager='apk'
+      package_manager_install_cmd=('--update' '--no-cache' 'add')
+      package_manager_update_cmd=()
+      ;;
+    *rhel*)
+      packages+=()
+      package_manager='dnf'
+      package_manager_install_cmd=('install' '-y')
+      package_manager_update_cmd=()
+      ;;
+    *)
+      echo "This script doesn't officially support your distro"
+      exit 1
+  esac
+
+  if [[ -n "${package_manager_update_cmd[*]}" ]] ; then
+    update_cmd=( "${install_cmd[@]}" "${package_manager}" "${package_manager_update_cmd[@]}" )
+    printf '\nusing this command to update your system: %s\n' "${update_cmd[*]}"
+    "${update_cmd[@]}"
+  fi
+
+  need_to_install=''
+  needs=()
+  for package in "${packages[@]}"; do
+    bin_provided="${package##*|}"
+    package_name="${package%%|*}"
+    if ! command -v "${bin_provided:-${package_name}}" > /dev/null ; then
+      needs+=("${package_name}")
+      need_to_install='true'
+    fi
+  done
+
+  install_cmd=("${install_cmd[@]}" "${package_manager}" "${package_manager_install_cmd[@]}")
+
+  if [[ -n "${need_to_install}" ]]; then
+    printf 'need to install: %s\n' "${needs[@]}"
+    printf '\nusing this command to install it: %s %s\n' "${install_cmd[*]}" "${needs[*]}"
+    "${install_cmd[@]}" "${needs[@]}"
+  fi
+
+}
+
 function check_os(){
   ID_LIKE="$(source /etc/os-release && echo "${ID_LIKE}")"
   case "${ID_LIKE:-}" in
@@ -39,8 +102,9 @@ function install_pip3(){
 
 # https://elrey.casa/bash/scripting/main
 function main(){
-  check_os
-  install_deps
+  # check_os
+  # install_deps
+  deps_install
   install_pip3
 }
 
