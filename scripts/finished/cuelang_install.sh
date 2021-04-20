c63e1b6b8fad0b1fdc75259326ae921474a59f99
elreydetoda <unc741@gmail.com>
2021-04-20 19:51:48 -0400

    adding checksum check

--- scripts/finished/cuelang_install.sh
+++ scripts/finished/cuelang_install.sh
@@ -14,11 +14,13 @@ function get_latest_stable_version(){
 
 function install_cue(){
   # https://github.com/cuelang/cue/releases/download/v0.3.2/cue_v0.3.2_linux_amd64.tar.gz
-  install <(
-    tar -Oxzvf <(
-      curl -fsSL "https://${base_domain}/${org_name}/${proj_name}/releases/download/${stable_version}/cue_${stable_version}_linux_amd64.tar.gz"
-    ) cue
-  ) ./cue
+  cue_bin="$(curl -fsSL "https://${base_domain}/${org_name}/${proj_name}/releases/download/${stable_version}/${cue_archive}" | base64 -w 0)"
+  current_checksum="$(curl -fsSL "https://${base_domain}/${org_name}/${proj_name}/releases/download/${stable_version}/checksums.txt" | grep linux_amd64 | awk '{print $1}')"
+  bin_sha="$(base64 -d <<< "${cue_bin}" | sha256sum | awk '{print $1}')"
+
+  diff -s <(echo "${bin_sha}") <(echo "${current_checksum}")
+
+  install <( tar -Oxzvf <( base64 -d <<< "${cue_bin}" ) cue) ./cue
   sudo mv ./cue /usr/local/bin/
 }
 
@@ -27,6 +29,7 @@ function main(){
   org_name='cuelang'
   proj_name='cue'
   get_latest_stable_version
+  cue_archive="cue_${stable_version}_linux_amd64.tar.gz"
   install_cue
 }
 
